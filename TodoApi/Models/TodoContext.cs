b5833b73c9662f9aec8186ed13596250a1487546
Aanya2 <manav01patel@gmail.com>
2024-02-16 15:34:41 -0500

    Db error solved

--- TodoApi/Models/TodoContext.cs
+++ TodoApi/Models/TodoContext.cs
@@ -8,8 +8,17 @@ namespace TodoAPI.Models
     {
         public TodoContext(DbContextOptions<TodoContext> options) : base(options)
         {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "TodoList.db");
+
         }
 
         public DbSet<TodoItem> TodoItems { get; set; }
+        public string DbPath { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
     }
 }
